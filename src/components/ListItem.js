0b7a7fd3b50fb50625c93ef90661da559d2f3d90
Adit Modhvadia <adit.modhvadia@gmail.com>
2019-04-08 01:50:54 +0530

    Update ListItem.js

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,25 +2,26 @@ import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 const listItem = props => (
-<TouchableOpacity /* onPress={() => alert(props.onItemsPressed)} */
-onPress={() => props.onItemDeleted(props.index)} 
-    /* onLongPress={props.onLongPressed(i)} */>
+  <TouchableOpacity /* onPress={() => alert(props.onItemsPressed)} */
+    onPress={() => props.onItemDeleted(props.index)}
+    /* onLongPress={props.onLongPressed(i)} */
+  >
     <View style={styles.listItem}>
-    <Text>{props.firstName}</Text>
-    <Text>{props.index}</Text>
+      <Text>{props.firstName}</Text>
+      <Text>{props.index}</Text>
     </View>
-</TouchableOpacity>
+  </TouchableOpacity>
 );
 
 const styles = StyleSheet.create({
-listItem: {
+  listItem: {
     width: "100%",
     margin: 15,
     borderWidth: 2,
     borderColor: "#adad",
     padding: 10,
     backgroundColor: "#eee"
-}
+  }
 });
 
 export default listItem;
