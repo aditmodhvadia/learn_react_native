37b62e32845192030219ba06299c6d838ccf4081
Adit Modhvadia <adit.modhvadia@gmail.com>
2019-04-14 16:17:24 +0530

    Add react-native-vector-icons lib
    -Add delete button to delete item from list

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 const listItem = props => (
-  <TouchableOpacity /* onPress={() => alert(props.onItemsPressed)} */
-    onPress={() => props.onItemDeleted(props.index)}
-    /* onLongPress={props.onLongPressed(i)} */
-  >
-    <View style={styles.listItem}>
-      <Text>{props.firstName}</Text>
-    </View>
-  </TouchableOpacity>
+  <View style={styles.listItem}>
+    <Text>{props.firstName}</Text>
+    <TouchableOpacity onPress={() => props.onItemDeleted(props.index)}>
+      <Icon size={30} name="delete" color="red" />
+    </TouchableOpacity>
+  </View>
 );
 
 const styles = StyleSheet.create({
   listItem: {
     width: "100%",
+    flexDirection: "row",
     margin: 15,
     padding: 10,
+    justifyContent: "space-evenly",
     backgroundColor: "#eee"
   }
 });
